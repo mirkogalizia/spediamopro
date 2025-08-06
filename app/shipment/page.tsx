5e6009b49915566348d193945fa0b8e6eae76ccc
mirkogalizia <mirko.galizia@outlook.com>
2025-08-06 15:28:13 +0200

    prova

--- app/shipment/page.tsx
+++ app/shipment/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState, FormEvent } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../../lib/firebase";           // importa auth da lib/firebase
 import { onAuthStateChanged, User } from "firebase/auth";
 
