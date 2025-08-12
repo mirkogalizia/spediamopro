7635ef2b3ee75a68fc91cca27ad04b9cb3aae214
mirkogalizia <mirko.galizia@outlook.com>
2025-08-12 10:38:20 +0200

    ok

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import Sidebar from './components/Sidebar';
+import Sidebar from '@/components/Sidebar';
 
 const theme = createTheme({
   palette: {
