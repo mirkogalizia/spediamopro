93800ff42ceb4639b363fc84359623b1133b17f1
mirkogalizia <mirko.galizia@outlook.com>
2025-08-12 11:32:36 +0200

    prova

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
+import type { ClassValue } from "clsx"
+import clsx from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/** Unisce classi Tailwind evitando duplicati (usato da shadcn/ui) */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
