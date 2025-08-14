9447a72eaa893c88b48ee0b8d996824f278fcc9a
mirkogalizia <mirko.galizia@outlook.com>
2025-08-14 23:49:58 +0200

    prova

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.shopify.com'], // <-- aggiungi qui il dominio giusto
+  },
 };
 
 module.exports = nextConfig;
